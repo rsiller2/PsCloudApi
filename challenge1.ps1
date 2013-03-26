29065f8e0fd354a39dad68ad37f800fab48a2062
Rubin Siller <rubin.siller@rackspace.com>
2013-03-26 16:42:31 -0500

    my first commit

---
+++ challenge1.ps1
@@ -0,0 +1,79 @@
+﻿Import-Module $PSScriptRoot\get_token.psm1
+Get-Token
+
+$imageID = '8a3a9f96-b997-46fd-b7a8-a9e740796ffd'#Ubuntu12
+$flavor = 2
+$servercount = 2
+$commonname = 'apitest'
+$endpoint = 'https://dfw.servers.api.rackspacecloud.com/v2'
+$i = 1
+
+#Create Object Template and Array
+$objProto = New-Object PSObject
+$objProto | Add-Member –MemberType NoteProperty –Name Name –Value $null
+$objProto | Add-Member –MemberType NoteProperty –Name ID –Value $null
+$objProto | Add-Member –MemberType NoteProperty –Name AdminPass –Value $null
+$objProto | Add-Member –MemberType NoteProperty –Name PublicIP –Value $null
+$objProto | Add-Member –MemberType NoteProperty –Name PrivateIP –Value $null
+$objArray = @()
+
+#start of while loop
+while ($i -le $servercount){
+$name = $commonname + $i
+$string0 = '{
+    "server" : {
+        "name" : "$name",
+        "imageRef" : "$imageID",
+        "flavorRef" : "$flavor"
+    }
+}'
+
+
+
+$htoken = @{"X-Auth-Token" = "$token"}
+$url = '$endpoint/$custDDI/servers'
+$url = $ExecutionContext.InvokeCommand.ExpandString($url)
+$string = $ExecutionContext.InvokeCommand.ExpandString($string0)
+
+$request = Invoke-RestMethod -uri  $url -Method POST -Body $string -ContentType application/json -Headers $htoken
+$serverid = $request.server.id
+$adminpass = $request.server.adminPass
+
+"Name: " + $name
+"Server ID: " + $serverid
+"Admin Pass: " + $adminpass
+
+#create temp object and insert current info
+$objTemp = $objProto | Select-Object *
+$objTemp.Name = $name
+$objTemp.ID = $serverid
+$objTemp.AdminPass = $adminpass
+$objTemp.PublicIP = $pubicIP
+$objTemp.PrivateIP = $privateIP
+
+#add object to array of objects
+$objArray += $objTemp
+$i++}# end of while loop
+
+"Please wait while we get the IP"
+Start-Sleep -s 45 #need to add a retry if any IPs come back blank will probably need to add them to an array
+#start of while loop to get IP
+$j = 1
+while ($j -le $servercount){
+$url2 = $url + '/detail?name='+ '$commonname' + '$j'
+$url2 = $ExecutionContext.InvokeCommand.ExpandString($url2)
+$request2 = Invoke-RestMethod -uri  $url2 -Headers $htoken
+
+"name: "+ $request2.servers.name
+"id: "+ $request2.servers.id
+"Public: " + $request2.servers.addresses.public.addr
+"Prifate: " + $request2.servers.addresses.private.addr
+"flavor: "+ $request2.servers.flavor.id + " progress: " + $request2.servers.progress
+"`n"
+
+#junk will later try to add to the array of objects
+#$objArray[i-1].PublicIP = $PublicIP
+#$objArray[i-1].PrivateIP = $PrivateIP
+#$arrayname = $objArray.name
+$j++
+}
---
+++ challenge2.ps1
@@ -0,0 +1,96 @@
+﻿Import-Module $PSScriptRoot\get_token.psm1 
+Get-Token
+$imagename = 'testimage'
+$serverID ='01f8d216-91cc-4910-b08e-c0cf5a4e51a1'
+$string0 = '{
+"createImage" : {
+"name" : "$imagename",
+"metadata": {
+"ImageType": "test"
+}
+}
+}'
+$string = $ExecutionContext.InvokeCommand.ExpandString($string0)
+$endpoint = 'https://dfw.servers.api.rackspacecloud.com/v2'
+$htoken = @{"X-Auth-Token" = "$token"}
+$url = '$endpoint/$DDI/servers/$serverID/action'
+$url = $ExecutionContext.InvokeCommand.ExpandString($url)
+$request = Invoke-WebRequest -uri $url -Method Post -Body $string -ContentType application/json -Headers $htoken
+$request  |Format-List -Property RawContent
+
+$url2 = '$endpoint/$DDI/images/detail?server=$serverID&name=$imagename'
+$url2 = $ExecutionContext.InvokeCommand.ExpandString($url2)
+$request2 = Invoke-RestMethod -uri  $url2 -Headers $htoken
+
+$name = $request2.images.name
+$id = $request2.images.id
+$created = $request2.images.created
+$status = $request2.images.status
+$progress = $request2.images.progress
+
+'Name: ' + $name
+'ID: ' + $id
+'Created: ' + $created
+'Status: ' + $status
+'Progress: ' + $progress
+
+$timeout = new-timespan -Minutes 60
+$sw = [diagnostics.stopwatch]::StartNew()
+while ($sw.elapsed -lt $timeout){
+    if (($progress -eq 100) -and ($status -eq "Active")){
+        "Complete"
+        
+        $imageID = $request2.images.id
+        $flavor = 2
+        $newservername = 'apitestc2'
+        $string1 = '{
+        "server" : {
+        "name" : "$newservername",
+        "imageRef" : "$imageID",
+        "flavorRef" : "$flavor"
+        }
+     }'
+$string2 = $ExecutionContext.InvokeCommand.ExpandString($string1)
+$url3 = '$endpoint/$custDDI/servers'
+$url3 = $ExecutionContext.InvokeCommand.ExpandString($url3)
+$request3 = Invoke-RestMethod -uri  $url3 -Method POST -Body $string2 -ContentType application/json -Headers $htoken
+$newserverid = $request3.server.id
+$adminpass = $request3.server.adminPass
+
+"Name: " + $newservername
+"Server ID: " + $newserverid
+"Admin Pass: " + $adminpass
+
+"Please wait while we get the IP"
+Start-Sleep -s 45
+
+$url4 = '$endpoint/$DDI/servers/$newserverID'
+$url4 = $ExecutionContext.InvokeCommand.ExpandString($url4)
+$request4 = Invoke-RestMethod -uri  $url4 -Headers $htoken
+$newserver = $request4.server
+"name: "+ $newserver.name
+"status: "  + $newserver.status + " " + "progress: " + $newserver.progress
+"id: "+ $newserver.id
+"Public IP: "+ $newserver.addresses.public.addr
+"Private IP: "+ $newserver.addresses.private.addr
+"flavor: "+ $newserver.flavor.id + " metadata: " + $newserver.metadata
+        return
+          $sw.Stop()
+        }
+    else{
+    $request2 = Invoke-RestMethod -uri  $url2 -Headers $htoken
+
+$name = $request2.images.name
+$id = $request2.images.id
+$created = $request2.images.created
+$status = $request2.images.status
+$progress = $request2.images.progress
+    
+    write-host $status $progress}
+        
+    
+    start-sleep -seconds 20
+}
+ 
+write-host "Timed out"
+  $sw.Stop()
