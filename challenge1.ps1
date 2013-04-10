939427a3d68670331f0c28ccd41fe56f370a1236
Rubin Siller <rubin.siller@rackspace.com>
2013-04-09 22:19:29 -0500

    final4challenge

--- challenge1.ps1
+++ challenge1.ps1
@@ -1,12 +1,63 @@
-﻿Import-Module $PSScriptRoot\get_token.psm1
-Get-Token
+﻿#variables
 
 $imageID = '8a3a9f96-b997-46fd-b7a8-a9e740796ffd'#Ubuntu12
 $flavor = 2
 $servercount = 2
-$commonname = 'apitest'
+$commonname = 'finaltest'
 $endpoint = 'https://dfw.servers.api.rackspacecloud.com/v2'
 $i = 1
+
+#load ini
+# This code assumes that no blank lines are in the file--a blank line will cause an early termination of the read loop
+#ini values should not be in quotes it will break the request
+# Confirm that the file exists on disk
+
+$IniFile_NME="$PSScriptRoot\auth.ini"
+
+dir $IniFile_NME
+
+# Parse the file
+
+$InputFile = [System.IO.File]::OpenText("$IniFile_NME")
+
+while($InputRecord = $InputFile.ReadLine())
+    {
+
+        # Determine the position of the equal sign (=)
+
+        $Pos = $InputRecord.IndexOf('=')
+        
+
+        # Determine the length of the record
+
+        $Len = $InputRecord.Length
+        
+
+        # Parse the record
+
+        $Variable_NME = $InputRecord.Substring(1, $Pos -1)
+        $VariableValue_STR = $InputRecord.Substring($Pos + 1, $Len -$Pos -1)
+
+        # Create a new variable based on the parsed information
+
+        new-variable -name $Variable_NME -value $VariableValue_STR 
+        get-variable -name $Variable_NME
+    }
+#close ini
+$InputFile.Close()
+
+#authenticate and return ddi and token
+#string
+$string = '{"auth" : {"RAX-KSKEY:apiKeyCredentials" : {"username" : "$user", "apiKey" : "$apikey"}}}'
+
+#expands variables within string
+$post = $ExecutionContext.InvokeCommand.ExpandString($string) 
+#sets request to variable to be used later
+$request = Invoke-RestMethod -uri $authurl -Method Post -Body $post -ContentType application/json
+
+$token = $request.access.token.id 
+$custDDI = $request.access.token.tenant.id
+
 
 #Create Object Template and Array
 $objProto = New-Object PSObject
@@ -17,63 +68,58 @@ $objProto | Add-Member –MemberType NoteProperty –Name PublicIP –Value $nul
 $objProto | Add-Member –MemberType NoteProperty –Name PrivateIP –Value $null
 $objArray = @()
 
-#start of while loop
+#start of while loop to build servers
 while ($i -le $servercount){
-$name = $commonname + $i
-$string0 = '{
+    $name = $commonname + $i
+    $string = '{
     "server" : {
         "name" : "$name",
         "imageRef" : "$imageID",
         "flavorRef" : "$flavor"
     }
-}'
+    }'
 
+    $htoken = @{"X-Auth-Token" = "$token"}
+    $url = '$endpoint/$custDDI/servers'
+    $url = $ExecutionContext.InvokeCommand.ExpandString($url)
+    $string = $ExecutionContext.InvokeCommand.ExpandString($string)
 
+    $request = Invoke-RestMethod -uri  $url -Method POST -Body $string -ContentType application/json -Headers $htoken
+    $serverid = $request.server.id
+    $adminpass = $request.server.adminPass
 
-$htoken = @{"X-Auth-Token" = "$token"}
-$url = '$endpoint/$custDDI/servers'
-$url = $ExecutionContext.InvokeCommand.ExpandString($url)
-$string = $ExecutionContext.InvokeCommand.ExpandString($string0)
+    "Name: " + $name
+    "Server ID: " + $serverid
+    "Admin Pass: " + $adminpass
 
-$request = Invoke-RestMethod -uri  $url -Method POST -Body $string -ContentType application/json -Headers $htoken
-$serverid = $request.server.id
-$adminpass = $request.server.adminPass
+    #create temp object and insert current info
+    $objTemp = $objProto | Select-Object *
+    $objTemp.Name = $name
+    $objTemp.ID = $serverid
+    $objTemp.AdminPass = $adminpass
+    $objTemp.PublicIP = $pubicIP
+    $objTemp.PrivateIP = $privateIP
 
-"Name: " + $name
-"Server ID: " + $serverid
-"Admin Pass: " + $adminpass
-
-#create temp object and insert current info
-$objTemp = $objProto | Select-Object *
-$objTemp.Name = $name
-$objTemp.ID = $serverid
-$objTemp.AdminPass = $adminpass
-$objTemp.PublicIP = $pubicIP
-$objTemp.PrivateIP = $privateIP
-
-#add object to array of objects
-$objArray += $objTemp
-$i++}# end of while loop
+    #add object to array of objects
+    $objArray += $objTemp
+    $i++}
+# end of while loop
 
 "Please wait while we get the IP"
-Start-Sleep -s 45 #need to add a retry if any IPs come back blank will probably need to add them to an array
+
+Start-Sleep -s 90 
 #start of while loop to get IP
 $j = 1
 while ($j -le $servercount){
-$url2 = $url + '/detail?name='+ '$commonname' + '$j'
-$url2 = $ExecutionContext.InvokeCommand.ExpandString($url2)
-$request2 = Invoke-RestMethod -uri  $url2 -Headers $htoken
+    $url2 = $url + '/detail?name='+ '$commonname' + '$j'
+    $url2 = $ExecutionContext.InvokeCommand.ExpandString($url2)
+    $request2 = Invoke-RestMethod -uri  $url2 -Headers $htoken
 
-"name: "+ $request2.servers.name
-"id: "+ $request2.servers.id
-"Public: " + $request2.servers.addresses.public.addr
-"Prifate: " + $request2.servers.addresses.private.addr
-"flavor: "+ $request2.servers.flavor.id + " progress: " + $request2.servers.progress
-"`n"
-
-#junk will later try to add to the array of objects
-#$objArray[i-1].PublicIP = $PublicIP
-#$objArray[i-1].PrivateIP = $PrivateIP
-#$arrayname = $objArray.name
-$j++
+    "name: "+ $request2.servers.name
+    "id: "+ $request2.servers.id
+    "Public: " + $request2.servers.addresses.public.addr
+    "Private: " + $request2.servers.addresses.private.addr
+    "flavor: "+ $request2.servers.flavor.id + " progress: " + $request2.servers.progress
+    "`n"
+    $j++
 }
--- challenge2.ps1
+++ challenge2.ps1
@@ -1,32 +1,98 @@
-﻿Import-Module $PSScriptRoot\get_token.psm1 
-Get-Token
-$imagename = 'testimage'
-$serverID ='01f8d216-91cc-4910-b08e-c0cf5a4e51a1'
-$string0 = '{
-"createImage" : {
-"name" : "$imagename",
-"metadata": {
-"ImageType": "test"
-}
-}
-}'
-$string = $ExecutionContext.InvokeCommand.ExpandString($string0)
-$endpoint = 'https://dfw.servers.api.rackspacecloud.com/v2'
+﻿
+#variables to create image
+    $imagename = 'testimage'
+    $serverID ='90ce0d19-7455-4ffa-a57f-3468afbd15c5'
+    $endpoint = 'https://dfw.servers.api.rackspacecloud.com/v2'
+    
+#variables used to create server
+    $flavor = 2
+    $newservername = 'finaltest2'
+    
+#load ini
+# This code assumes that no blank lines are in the file--a blank line will cause an early termination of the read loop
+#ini values should not be in quotes it will break the request
+# Confirm that the file exists on disk
+
+$IniFile_NME="$PSScriptRoot\auth.ini"
+
+dir $IniFile_NME
+
+# Parse the file
+
+$InputFile = [System.IO.File]::OpenText("$IniFile_NME")
+
+while($InputRecord = $InputFile.ReadLine())
+    {
+
+        # Determine the position of the equal sign (=)
+
+        $Pos = $InputRecord.IndexOf('=')
+        
+
+        # Determine the length of the record
+
+        $Len = $InputRecord.Length
+        
+
+        # Parse the record
+
+        $Variable_NME = $InputRecord.Substring(1, $Pos -1)
+        $VariableValue_STR = $InputRecord.Substring($Pos + 1, $Len -$Pos -1)
+
+        # Create a new variable based on the parsed information
+
+        new-variable -name $Variable_NME -value $VariableValue_STR -force
+        get-variable -name $Variable_NME
+    }
+#close ini
+$InputFile.Close()
+
+#authenticate and return ddi and token
+#string
+$string = '{"auth" : {"RAX-KSKEY:apiKeyCredentials" : {"username" : "$user", "apiKey" : "$apikey"}}}'
+
+#expands variables within string
+$post = $ExecutionContext.InvokeCommand.ExpandString($string) 
+
+#sets request to variable to be used later
+$request = Invoke-RestMethod -uri $authurl -Method Post -Body $post -ContentType application/json
+
+$token = $request.access.token.id 
+$custDDI = $request.access.token.tenant.id
+
+#reqest to create image
+$string = '{
+    "createImage" : {
+    "name" : "$imagename",
+    "metadata": {
+    "ImageType": "test"
+    }
+    }
+    }'
+$string = $ExecutionContext.InvokeCommand.ExpandString($string)
 $htoken = @{"X-Auth-Token" = "$token"}
-$url = '$endpoint/$DDI/servers/$serverID/action'
+$url = '$endpoint/$custDDI/servers/$serverID/action'
 $url = $ExecutionContext.InvokeCommand.ExpandString($url)
 $request = Invoke-WebRequest -uri $url -Method Post -Body $string -ContentType application/json -Headers $htoken
 $request  |Format-List -Property RawContent
 
-$url2 = '$endpoint/$DDI/images/detail?server=$serverID&name=$imagename'
+#request to get image status
+$url2 = '$endpoint/$custDDI/images/detail?server=$serverID&name=$imagename'
 $url2 = $ExecutionContext.InvokeCommand.ExpandString($url2)
 $request2 = Invoke-RestMethod -uri  $url2 -Headers $htoken
-
 $name = $request2.images.name
 $id = $request2.images.id
 $created = $request2.images.created
 $status = $request2.images.status
 $progress = $request2.images.progress
+$imageID = $request2.images.id
+$string2 = '{
+    "server" : {
+    "name" : "$newservername",
+    "imageRef" : "$imageID",
+    "flavorRef" : "$flavor"
+    }
+    }'
 
 'Name: ' + $name
 'ID: ' + $id
@@ -34,63 +100,59 @@ $progress = $request2.images.progress
 'Status: ' + $status
 'Progress: ' + $progress
 
+#loop to check image status and then build server
 $timeout = new-timespan -Minutes 60
 $sw = [diagnostics.stopwatch]::StartNew()
 while ($sw.elapsed -lt $timeout){
     if (($progress -eq 100) -and ($status -eq "Active")){
         "Complete"
-        
-        $imageID = $request2.images.id
-        $flavor = 2
-        $newservername = 'apitestc2'
-        $string1 = '{
-        "server" : {
-        "name" : "$newservername",
-        "imageRef" : "$imageID",
-        "flavorRef" : "$flavor"
+    
+    $string2 = $ExecutionContext.InvokeCommand.ExpandString($string2)
+    #request to create server
+    
+    $url3 = '$endpoint/$custDDI/servers'
+    $url3 = $ExecutionContext.InvokeCommand.ExpandString($url3)
+    $request3 = Invoke-RestMethod -uri  $url3 -Method POST -Body $string2 -ContentType application/json -Headers $htoken
+    $newserverid = $request3.server.id
+    $adminpass = $request3.server.adminPass
+
+    "Name: " + $newservername
+    "Server ID: " + $newserverid
+    "Admin Pass: " + $adminpass
+
+    "Please wait while we get the IP"
+    Start-Sleep -s 90
+    #request to create server
+    $url4 = '$endpoint/$custDDI/servers/$newserverID'
+    $url4 = $ExecutionContext.InvokeCommand.ExpandString($url4)
+    $request4 = Invoke-RestMethod -uri $url4 -Headers $htoken
+    $newserver = $request4.server
+    "name: "+ $newserver.name
+    "status: "  + $newserver.status + " " + "progress: " + $newserver.progress
+    "id: "+ $newserver.id
+    "Public IP: "+ $newserver.addresses.public.addr
+    "Private IP: "+ $newserver.addresses.private.addr
+    "flavor: "+ $newserver.flavor.id + " metadata: " + $newserver.metadata
+    return
+    $sw.Stop()
         }
-     }'
-$string2 = $ExecutionContext.InvokeCommand.ExpandString($string1)
-$url3 = '$endpoint/$custDDI/servers'
-$url3 = $ExecutionContext.InvokeCommand.ExpandString($url3)
-$request3 = Invoke-RestMethod -uri  $url3 -Method POST -Body $string2 -ContentType application/json -Headers $htoken
-$newserverid = $request3.server.id
-$adminpass = $request3.server.adminPass
 
-"Name: " + $newservername
-"Server ID: " + $newserverid
-"Admin Pass: " + $adminpass
 
-"Please wait while we get the IP"
-Start-Sleep -s 45
-
-$url4 = '$endpoint/$DDI/servers/$newserverID'
-$url4 = $ExecutionContext.InvokeCommand.ExpandString($url4)
-$request4 = Invoke-RestMethod -uri  $url4 -Headers $htoken
-$newserver = $request4.server
-"name: "+ $newserver.name
-"status: "  + $newserver.status + " " + "progress: " + $newserver.progress
-"id: "+ $newserver.id
-"Public IP: "+ $newserver.addresses.public.addr
-"Private IP: "+ $newserver.addresses.private.addr
-"flavor: "+ $newserver.flavor.id + " metadata: " + $newserver.metadata
-        return
-          $sw.Stop()
-        }
     else{
+    
+    #reqeust to get image status
     $request2 = Invoke-RestMethod -uri  $url2 -Headers $htoken
 
-$name = $request2.images.name
-$id = $request2.images.id
-$created = $request2.images.created
-$status = $request2.images.status
-$progress = $request2.images.progress
-    
+    $name = $request2.images.name
+    $id = $request2.images.id
+    $created = $request2.images.created
+    $status = $request2.images.status
+    $progress = $request2.images.progress
     write-host $status $progress}
         
     
     start-sleep -seconds 20
-}
+    }
  
 write-host "Timed out"
-  $sw.Stop()
+$sw.Stop()
